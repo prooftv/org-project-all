080f2c0336128c219da7f03f7854815571e81be0
prooftv <myinbox49@live.com>
2023-05-22 15:35:48 +0200

    Update sanityClient.ts

--- web/lib/sanityClient.ts
+++ web/lib/sanityClient.ts
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "e78eosta",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-05-25",
   useCdn: true
